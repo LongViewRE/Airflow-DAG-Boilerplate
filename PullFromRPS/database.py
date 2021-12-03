46ce0db9dc9985f361379e8106a29cd8cb745566
Eishitha Galpaya <egalpaya@gmail.com>
2021-12-03 12:41:58 +1100

    added functions to pull and format data from gerald

--- PullFromRPS/database.py
+++ PullFromRPS/database.py
@@ -3,7 +3,7 @@ import os
 from LV_db_connection import GremlinClient
 from dotenv import load_dotenv
 
-def get_gerald_properties(client):
+def get_gerald_property_ids(client):
     """
     Returns a list of property IDs of properties under management
     Gerald.
@@ -18,89 +18,46 @@ def get_all_property_info_gerald(client, prop_id):
     that which was already present in gerald.
     """
     query = f"""g.V().hasLabel('property').has('id', eq('{prop_id}'))
-                .union(identity(),
-                __.in('Owns').as('landlord').in('is a').as('contact').select('landlord','contact'),
-                __.in('renting').as('tenancy').in('is a').as('contact').select('tenancy','contact'))
-                .dedup()
+                .in('Owns','renting').hasLabel('landlord','tenant')
+                .in('is a').dedup().tree()
                 """
-                ### TODO - find better query to return an easier format
+    res = client.submit(query)
 
-def format_property_info(result):
-    """
-    Formats gerald output """
+    property = format_property_info(client, res)
+    return property
 
+def format_property_info(client, result):
+    """
+    Formats gerald output of all property info. The gremlin query
+    returns the output as a tree with root as property, first level
+    children as tenancy/landlord, second level children as contacts.
+    This function parses this tree and returns more readable 
+    nested representation similar to that returned by the API.
+    """
+    result = result[0].popitem()[1]
+    property = client.parse_graph_json(result['key'])
 
-def get_field(vertex, key):
-    """
-    Gets a value from the given graph output vertex that may or may not exist.
-    If it does not exist, returns None.
-    """
-    value = None
-    with contextlib.suppress(KeyError):
-        value = vertex["properties"][key][0]["value"]
-    return value
-
-def read_landlord(lan):
-    """
-    Parses landlord graph json representation returned by query and itself returns a dict
-    corresponding to the representation defined in the API. 
-    """
+    tenant = {}
     landlord = {}
-    landlord["id"] = lan["id"]
-    landlord["contacts"] = 
+    for item in result['value'].values():
+        if item['key']['label'] == 'tenant':
+            tenant = client.parse_graph_json(item['key'])
 
-def read_tenancy(ten):
-    """
-    Parses tenancy graph json representation returned by query and itself returns a dict
-    corresponding to the representation defined in the API. 
-    """  
-    tenancy = {}
-    tenancy["id"] = ten["id"]
-    tenancy["startDate"] = ten["properties"]["startDate"][0]["value"]
-    tenancy["endDate"] = ten["properties"]["endDate"][0]["value"]
-    tenancy["weeklyRent"] = ten["properties"]["weeklyRent"][0]["value"]
-    tenancy["status"] = ten["properties"]["status"][0]["value"]
-    tenancy["periodic"] = (get_field(ten, "periodic") == True)
-    tenancy["endConfirmed"] = (get_field(ten, "endConfirmed") == True)
+            tenant['contacts'] = []
+            for contact in item['value'].values():
+                con = client.parse_graph_json(contact['key'])
+                tenant['contacts'].append(con)
+            
+        if item['key']['label'] == 'landlord':
+            landlord = client.parse_graph_json(item['key'])
 
-    return tenancy
-
-def read_contact(con):
-    """
-    Parses contact graph json representation returned by query and itself returns a dict
-    corresponding to the representation defined in the API. 
-    """
-    contact = {}
-    contact["id"] = con["id"]
-
-    contact["first name"] = get_field(con, "first name")
-    contact["last name"] = get_field(con, "last name")
-    contact["Company Name"] = get_field(con, "Company Name")
-    contact["address"] = get_field(con, "address")
-    contact["email"] = get_field(con, "email")
-    contact["mobile"] = get_field(con, "mobile")
-
-    return contact
-
-
-def read_property(prop):
-    """
-    Parses property graph json representation returned by query and itself returns a dict
-    corresponding to the representation defined in the API. 
-    """
-    property = {}
-    property["ID"] = prop["id"]
-    property["housenumber"] =  prop["properties"]["housenumber"][0]["value"]
-    property["address1"] = prop["properties"]["address1"][0]["value"]
-    property["address3"] = prop["properties"]["address3"][0]["value"]
-    property["postcode"] = prop["properties"]["postcode"][0]["value"]
-    property["address4"] = prop["properties"]["address4"][0]["value"]
-
-    property["latitude"] = get_field(prop, "latitude")
-    property["longitude"] = get_field(prop, "longitude")
-    property["pm"] = get_field(prop, "pm")
-    property["amf"] = get_field(prop, "amf")
-    property["type"] = get_field(prop, "type")
+            landlord['contacts'] = []
+            for contact in item['value'].values():
+                con = client.parse_graph_json(contact['key'])
+                landlord['contacts'].append(con)
+    
+    property["landlord"] = landlord
+    property["tenancy"] = tenant
 
     return property
 
