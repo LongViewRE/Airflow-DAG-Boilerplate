f2bf0ec57d750f10f5f2f82f28d4eae0b598e328
jakehattis <jacobhattis@gmail.com>
2022-01-21 09:45:16 +1100

    refactored azure_syncing

--- gerald_syncing/PullFromAzure/gerald.py
+++ gerald_syncing/PullFromAzure/gerald.py
@@ -1,36 +1,13 @@
-def get_employees(employees_json):
-    employees = []
-        
-    for i in employees_json:
-        #if statement to check if employee is still active
-
-
-        for key in i:
-            if i[key] is None:
-                i[key] = ''
-            if "'" in i[key]:
-                i[key] = i[key].replace("'", "")
-            
-            # active status is decided by if employee has an assigned licenses - 0 licenses = not active 
-            if (len(i['assignedLicenses']) == 0):
-                i['active'] = False
-            else:
-                i['active'] = True
-
-            
-            #add only internal employees to table not external parties.
-            if 'longview.com.au' in i['mail']:
-                employees.append(i)
-            
-    return employees
-
-def format_queries(employees):
+def format_queries(gerald_client, employees):
+    '''
+    Function to format employee queries for Gerald. 
+    '''
     empV = []
     for e in employees:
         # id, email, first name, last name, active 
         #the below query returns all associated properties of an object given an id
         query = f"g.V('emp-{e['id']}')"
-        call = self.Gerald.submit(query)
+        call = gerald_client.submit(query)
         #call to gerald 
         #if employee is not in gerald
         if len(call) == 0:
@@ -40,3 +17,25 @@ def format_queries(employees):
             new = f"g.V('emp-{e['id']}').property('firstName', '{e['givenName']}').property('lastName', '{e['surname']}').property('email', '{e['mail']}').property('active', '{e['active' ]}').property('mobile', '{e['mobilePhone']}')"
             empV.append(query)
     return empV
+
+
+def format_queries(gerald_client, employees):
+    '''
+    Function to format employee queries for Gerald. 
+    '''
+    empV = []
+    for e in employees:
+        # id, email, first name, last name, active 
+        #the below query returns all associated properties of an object given an id
+        query = f"g.V('emp-{e['id']}')"
+        call = gerald_client.submit(query)
+        #call to gerald 
+        #if employee is not in gerald
+        if len(call) == 0:
+            new = f"g.addV('emp-{e['id']}').property('firstName', '{e['givenName']}').property('lastName', '{e['surname']}').property('email', '{e['mail']}').property('active', '{e['active']}').property('mobile', '{e['mobilePhone']}')"
+            empV.append(new)
+        else:
+            new = f"g.V('emp-{e['id']}').property('firstName', '{e['givenName']}').property('lastName', '{e['surname']}').property('email', '{e['mail']}').property('active', '{e['active' ]}').property('mobile', '{e['mobilePhone']}')"
+            empV.append(query)
+    return empV
+
--- gerald_syncing/PullFromAzure/sync.py
+++ gerald_syncing/PullFromAzure/sync.py
@@ -9,15 +9,16 @@ from LV_db_connection import GremlinClient
 from LV_db_connection import connect_sql_app
 from LV_external_services import MSGraphClient
 
-from gerald_syncing.PullFromAzure.gerald import get_employees, format_queries
-from gerald_syncing.PullFromAzure.update_gr import gr_create_employees, gr_missing_employees
-from gerald_syncing.PullFromAzure.update_artemis import appr_create_employees, appr_missing_employees
+from gerald_syncing.PullFromAzure.azure import get_azure_employees
+from gerald_syncing.PullFromAzure.gerald import format_queries
+from gerald_syncing.PullFromAzure.sql import get_sql_employees, missing_sql_employees, sql_create_employees
 
 """
 Class that pulls employees from Gerald, checks if all employees are accounted for and creates
 new nodes for employees that do not exist in Gerald
 """
 class PullFromAzureFacade():
+
     def __init__(self) -> None:
         gerald_username = os.environ['gerald_username']
         gerald_password=  os.environ['gerald_password']
@@ -27,40 +28,48 @@ class PullFromAzureFacade():
         self.Gerald = GremlinClient(gerald_username, gerald_password)
         self.AzureAD = MSGraphClient(azure_username, azure_password)
         self.grcursor = connect_sql_app('GuaranteedRent')
-    
+        self.apprcursor = connect_sql_app('ArtemisDB')
+
     def pull(self):
         """
-        Pulls information from Gerald and stores it in a tmp file
+        Pulls employee information and stores it in a tmp file
         """
-        azure_emps = self.AzureAD.retrieve_employees()['value']
-        gerald_emps = get_employees(azure_emps)
-        
 
-        with open("/tmpdata/Employees_gerald.json", "w") as f:
-            json.dump(gerald_emps, f, indent=4)
+        #Employees in Azure AD
+        azure_emps = get_azure_employees(self.AzureAD)
+        
+        with open("/tmpdata/Employees_azure.json", "w") as f:
+            json.dump(azure_emps, f, indent=4)
     
-        gr_emps = self.grcursor.execute("SELECT * FROM employees")
+
+        #Employees from the GR database
+        gr_emps = get_sql_employees(self.grcursor, 'gr')
         with open("/tmpdata/Employees_gr.json", "w") as f:
             json.dump(gr_emps, f, indent=4)
 
+
+
     def process(self):
         """
-        Forms the queries for Gerald and saves it to a tmp file
+        Formats queries for Gerald  and saves it to a tmp file
         """
+
+        # Gerald Related Processing
         with open("/tmpdata/Employees_gerald.json", "r") as f:
             gerald_emps = json.load(f)
 
-        queries = []
-        queries = format_queries(gerald_emps)
+        queries = format_queries(self.Gerald, gerald_emps)
 
         with open("/tmpdata/Employees_geraldqueries.json", "w") as f:
             json.dump(queries, f, indent=4)
 
 
+        # GR Related Processing
         with open("/tmpdata/Employees_gr.json", "r") as f:
             gr_emps = json.load(f)
         
-        gr_missing = gr_missing_employees(gr_emps, gerald_emps)
+        gr_missing = missing_sql_employees(gr_emps, gerald_emps, database='gr')
+
         with open("/tmpdata/Employees_grmissing.json", "w") as f:
             json.dump(gr_missing, f, indent=4)
 
@@ -91,5 +100,6 @@ class PullFromAzureFacade():
             with open("/tmpdata/Employees_grmissing.json", "r") as f:
                 gr_missing = json.load(f)
 
-            gr_create_employees(gr_missing, self.grcursor)
+            sql_create_employees(self.grcursor, gr_missing, database='gr')
+
             logging.info("Submitted all queries")
