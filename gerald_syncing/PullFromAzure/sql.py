7999abbc34bfd040b91e254748f0fb1b38b3a92a
Adrija Banerjee <adrija.banerjee@longview.com.au>
2022-01-21 13:32:10 +1100

    changed email key error

--- gerald_syncing/PullFromAzure/sql.py
+++ gerald_syncing/PullFromAzure/sql.py
@@ -35,7 +35,7 @@ def missing_sql_employees(azure_emps, gr_emps = [], appr_emps = [], database = '
 
         for emp in azure_emps:
             for gr_emp in gr_emps:
-                if(emp['mail'] == gr_emp['emailID']):
+                if(emp['mail'] == gr_emp['email']):
                     emps_to_create.append(emp)
 
 
