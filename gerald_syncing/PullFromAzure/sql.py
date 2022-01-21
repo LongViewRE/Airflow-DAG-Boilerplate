808ef0f33fda2c5d40b501765f0bf56b50ffa7fe
Adrija Banerjee <adrija.banerjee@longview.com.au>
2022-01-21 14:55:59 +1100

    changed insert table query syntax

--- gerald_syncing/PullFromAzure/sql.py
+++ gerald_syncing/PullFromAzure/sql.py
@@ -57,8 +57,8 @@ def sql_create_employees(db_client, emps_to_create, database) -> None:
     if database == 'gr':
 
         for emp in emps_to_create:
-                db_client.execute(f"INSERT INTO employees ('employeeID', 'firstName', 'lastName', 'emailID') VALUES ('{emp['id']}', '{emp['givenName']}', '{emp['surname']}', '{emp['mail']}')")
-    
+                # db_client.execute(f"INSERT INTO employees ('employeeID', 'firstName', 'lastName', 'emailID') VALUES ('{emp['id']}', '{emp['givenName']}', '{emp['surname']}', '{emp['mail']}')")
+                db_client.execute(f"INSERT INTO employees ('firstName', 'lastName', 'emailID') VALUES ('{emp['givenName']}', '{emp['surname']}', '{emp['mail']}')")
     elif database == 'appraisals':
 
         for emp in emps_to_create:
