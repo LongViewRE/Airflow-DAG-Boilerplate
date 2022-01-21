473f98f11772f5228c3ac613ae6340c114b69d1b
Adrija Banerjee <adrija.banerjee@longview.com.au>
2022-01-21 13:51:53 +1100

    changed emp index

--- gerald_syncing/PullFromAzure/sql.py
+++ gerald_syncing/PullFromAzure/sql.py
@@ -57,7 +57,7 @@ def sql_create_employees(db_client, emps_to_create, database) -> None:
     if database == 'gr':
 
         for emp in emps_to_create:
-                db_client.execute(f"INSERT INTO TABLE employees VALUES ('{emp['id']}', '{emp['firstName']}', '{emp['lastName']}', '{emp['email']}')")
+                db_client.execute(f"INSERT INTO TABLE employees VALUES ('{emp['id']}', '{emp['firstname']}', '{emp['lastname']}', '{emp['mail']}')")
     
     elif database == 'appraisals':
 
